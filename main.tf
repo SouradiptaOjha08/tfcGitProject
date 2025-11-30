2dc0ce1434d500e20a5f2ed345f6d32a50c162a2
SouradiptaOjha08 <souradipta2025@gmail.com>
2025-11-30 09:47:32 +0000

    adding a terraform main file

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform { 
+  cloud { 
+    
+    organization = "Terraform_Demo_Organization" 
+
+    workspaces { 
+      name = "tfcDemo" 
+    } 
+  }
+ required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 5.0"
+
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+
+
+resource "aws_s3_bucket" "demo" {
+
+  bucket = "tfc-cli-demo-bucket-123"
+
+}
